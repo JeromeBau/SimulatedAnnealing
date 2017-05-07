7a3f21adf5c08d0b40fe6a35d4c7b43fd69277d6
Jerome Bau <JeromeBau@users.noreply.github.com>
2017-05-07 22:20:36 +0200

    Added docstrings

--- simple.py
+++ simple.py
@@ -8,7 +8,7 @@ from scipy import constants
 
 class SimulatedAnnealing(object):
     """
-    Class that applies Simulated Annealing to combinatoral optimization and visualisation on simple functions
+    Class that applies Simulated Annealing to a continuous optimization problem of a simple function
     """
     def __init__(self):
         self.kmax=100
@@ -19,25 +19,35 @@ class SimulatedAnnealing(object):
         self.cooling_rate = 0.9
 
     def energy(self, s):
+        """ State energery / Cost function """
         return 0.01*s**2 + 40 * np.sin(0.3*s)
 
     def neighbor(self, s):
-        if self.neighbor_function == 'neighbor':
+        """ Return a random neighbor
+        Possible neighbor functions:
+        (1) Uniform distributed random neighbor
+        (2) Normally distributed random neighbor
+        """
+        if self.neighbor_function == 'uniform':
             return s + random.uniform(-100, 100)
         elif self.neighbor_function == 'normal':
             return s + random.normal(0, 100)
 
     @staticmethod
     def acceptance_probability(energy_old, energy_new, T):
+        """ Returns an acceptance probability
+        Based on the comparison of old and new value and the according temperature
+        """"
         return np.exp((energy_old-energy_new)/T)
 
     def anneal(self, state):
+        """ Annealing process """
         self.state_evolution.append(state)
         energy_old = self.energy(state)
         T = self.temperature_initial
         while T > self.temperature_terminal:
             n = 1
-            while n <= 200:
+            while n < 200:
                 new_state = self.neighbor(state)
                 energy_new = self.energy(new_state)
                 if self.acceptance_probability(energy_old, energy_new, T) > random.random():
